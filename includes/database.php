0c2be44f2666f44d0e3f8951cdab3b0abf5e4354
ThomasBronola <89704625+ThomasBronola@users.noreply.github.com>
2022-06-14 17:29:57 +0800

    Added functions for login

--- includes/database.php
+++ includes/database.php
@@ -1,15 +1,15 @@
 <?php
 $servername = "localhost";
 $db_name = "brgy53";
+// $db_name = "capstone";
 $username = "root";
 $password = "";
 
 // Create connection
-$conn = new mysqli($servername, $db_name, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?>
