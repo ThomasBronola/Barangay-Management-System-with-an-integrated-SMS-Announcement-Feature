774aa4c2eec8b6899c9568605cd862e715c3a510
AgevEdz <jhonberniedevega@gmail.com>
2022-06-14 17:13:59 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,15 +1,14 @@
 <?php
 $servername = "localhost";
-$db_name = "brgy53";
 $username = "root";
 $password = "";
+$db_name = "brgy53";
 
 // Create connection
-$conn = new mysqli($servername, $db_name, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?>
