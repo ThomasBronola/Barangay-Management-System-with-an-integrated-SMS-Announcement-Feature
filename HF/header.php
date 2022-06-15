e2a8f70904336b492ed5ee66be838d0d0f0892f0
ThomasBronola <89704625+ThomasBronola@users.noreply.github.com>
2022-06-15 20:11:58 +0800

    Added Officials and Announcement Page

--- HF/header.php
+++ HF/header.php
@@ -1,8 +1,9 @@
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
-    <div class="container"><a class="navbar-brand" href="#">Brgy53</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+    <div class="container"><a class="navbar-brand" href="../layouts/landingpage.php">Brgy53</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto text-uppercase">
-                <li class="nav-item"><a class="nav-link" href="#team">Officials</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">HOME</a></li>
+                <li class="nav-item"><a class="nav-link" href="../layouts/about.php">Officials</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">contact</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Clearance</a></li>
                 <li class="nav-item"><a class="nav-link" href="#modal-login" data-bs-toggle="modal">LOGIN</a></li>
---
+++ layouts/about.php
@@ -0,0 +1,145 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Barangay Singko Tres</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/mstyles.css">
+    <script type="text/javascript" src="../assets/js/stickyscroll.js"></script>
+</head>
+
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
+    
+    <!-- HEADER FILE -->
+    <?php include '../HF/header.php';?>
+    <br> <br> <br> <br> <br>
+    <button onclick="topFunction()" id="btnScroll" title="Go to top" class="fa-solid fa-chevron-up"> </button>
+
+    <!-- CHAIRMAN  -->
+    <div class="container">    
+        <div class="col-lg-12 text-center">
+            <h2 class="text-uppercase">Official list</h2>
+            <h3 class="text-muted section-subheading">Brgy. Chairman</h3>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 offset-md-4 offset-lg-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Brgy. Chairman</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- KAGAWAD  -->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad<br /></p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+            <div class="col-sm-4 offset-md-4 offset-lg-4">
+                <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                    <h4>Jhon Bernie</h4>
+                    <p class="text-muted">Kagawad</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- SECRETARY AND TREASURER -->
+    <div class="container">
+        <div class="col-lg-12 text-center">
+            <h2 class="text-uppercase">SECREtary &amp; Treasurer</h2>
+            <div class="row">
+                <div class="col">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                        <h4>Jhon Bernie</h4>
+                        <p class="text-muted">Secretary</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                        <h4>Jhon Bernie</h4>
+                        <p class="text-muted">Treasurer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+    <!-- BRGY ADMIN AND SK CHAIRPERSON -->
+    <div class="container">
+        <div class="col-lg-12 text-center">
+            <h2 class="text-uppercase">Brgy. admin &amp; sk chairperson</h2>
+            <div class="row">
+                <div class="col">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                        <h4>Jhon Bernie</h4>
+                        <p class="text-muted">Brgy. Administrator</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="../assets/img/team/pakalu.jpg" />
+                        <h4>Jhon Bernie</h4>
+                        <p class="text-muted">SK Chairwoman</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+    <!-- SCRIPT FOR NAVIGATION TAB/HEADER ANIMATION -->
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/agency.js"></script>
+
+    <!-- FOOTER FILE  -->
+    <?php include '../HF/footer.php';?> 
+
+</body>
+
+</html>
